d1fa05d23cecb3b8fbc2401aefeaefea22bcac64
ccheroict <leadotc@gmail.com>
2020-08-05 15:14:14 +0200

    initial project

---
+++ src/main/java/com/ctran79/sscraperboot/SscraperBootApplication.java
@@ -0,0 +1,13 @@
+package com.ctran79.sscraperboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscraperBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SscraperBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ctran79/sscraperboot/SscraperBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctran79.sscraperboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SscraperBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
